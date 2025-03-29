967bae9389af7f469e4ee60a3a5b42746d2e7ece
Min Hee Jo <jom8be@bosch.com>
2025-03-29 14:28:53 +0100

    Add fault mode

--- scripts/process_json.sh
+++ scripts/process_json.sh
@@ -6,4 +6,4 @@ WORKING_DIR="$(dirname "$SCRIPT_DIR")"
 ### assumption: the json file is already created on the GPU
 
 # covert json files to strace command options
-python3 ${WORKING_DIR}/src/main.py
+python3 ${WORKING_DIR}/src/main.py --mode="error_code"
--- src/random_strace.py
+++ src/random_strace.py
@@ -4,19 +4,19 @@ import json
 import random
 import re
 
-x_min = 0
-x_max = 4294963200
-
-def get_random_number():
+def get_random_number(mode):
     """Generate a random unsigned integer."""
-    return random.randint(x_min, x_max)
+    if mode == "success":
+        return random.randint(0, 4294963200)
+    elif mode == "error_code":
+        return random.randint(0, 4095)
 
 
-def get_random_config(json_content):
+def get_random_config(json_content, mode):
     """Generate a randomly generated fault injection config file."""
     for fault in json_content["syslog_monitor_config"]["faults"]:
         # generate random number
-        random_number = get_random_number()
+        random_number = get_random_number(mode)
 
         # replace the return value with the random number
         output_str = re.sub(r"retval=\d+", f"retval={str(random_number)}", fault)
@@ -27,7 +27,7 @@ def get_random_config(json_content):
     return json_content
 
 
-def process_json_file(json_file_path):
+def process_json_file(json_file_path, mode):
     with open(json_file_path, 'r') as file:
         json_content = json.load(file)
 
@@ -36,7 +36,7 @@ def process_json_file(json_file_path):
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
 
     # generate random JSON content
-    random_json_content = get_random_config(json_content)
+    random_json_content = get_random_config(json_content, mode)
 
     # write random JSON content to file
     with open(output_file_path, 'w') as file:
@@ -45,37 +45,41 @@ def process_json_file(json_file_path):
     print(f"Generated JSON file: {output_file_path}")
 
 
-def process_run_directory(run_dir_path):
+def process_run_directory(run_dir_path, mode):
     """Process all json files in a run directory."""
     for filename in os.listdir(run_dir_path):
         if filename.endswith(".json"):
             json_file_path = os.path.join(run_dir_path, filename)
-            process_json_file(json_file_path)
+            process_json_file(json_file_path, mode)
                 
 
-def process_model_directory(model_dir_path):
+def process_model_directory(model_dir_path, mode):
     """Process all run directories in a model directory."""
     for run in os.listdir(model_dir_path):
         run_dir_path = os.path.join(model_dir_path, run)
         if os.path.isdir(run_dir_path):
-            process_run_directory(run_dir_path)
+            process_run_directory(run_dir_path, mode)
 
 
-def process_all_models(strace_dir):
+def process_all_models(strace_dir, mode):
     """Main function to process all model directories."""
     for model in os.listdir(strace_dir):
         model_dir_path = os.path.join(strace_dir, model)
         if os.path.isdir(model_dir_path):
-            process_model_directory(model_dir_path)
+            process_model_directory(model_dir_path, mode)
 
 
 if __name__ == "__main__":
     # parse command line arguments
     parser = argparse.ArgumentParser(description="Generate random strace fault injection parameters.")
     parser.add_argument("--config-dir-path", type=str, help="Path to the directory containing safety-fuzzing testbed config files (can be relative or absolute).")
+    parser.add_argument("--mode", type=str, required=True, help="Fault injection mode (e.g., 'error_code', 'success')")
     args = parser.parse_args()
+
+    mode = args.mode
+    print(f"Running in mode: {mode}")
 
     # get config directory path
     config_dir_path = os.path.abspath(args.config_dir_path)
 
-    process_all_models(config_dir_path)
+    process_all_models(config_dir_path, mode)
