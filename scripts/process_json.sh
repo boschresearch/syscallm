e5b8dd534900c916028ebb0136814b8bc004006f
Min Hee Jo <jom8be@bosch.com>
2025-04-03 22:17:42 +0200

    Support different mode

--- scripts/process_json.sh
+++ scripts/process_json.sh
@@ -6,4 +6,5 @@ WORKING_DIR="$(dirname "$SCRIPT_DIR")"
 ### assumption: the json file is already created on the GPU
 
 # covert json files to strace command options
-python3 ${WORKING_DIR}/src/main.py --mode="error_code"
+python3 ${WORKING_DIR}/src/main.py --mode="success"
+# python3 ${WORKING_DIR}/src/main.py --mode="error_code"
