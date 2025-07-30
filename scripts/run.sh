537896b2e574338f6eb9660712094f098f738808
Jo Min Hee <MinHee.Jo@de.bosch.com>
2025-07-30 12:56:37 +0200

    One script for all

---
+++ scripts/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORKING_DIR="$(dirname "$SCRIPT_DIR")"
+
+# check if nvidia-smi is available
+if command -v nvidia-smi &> /dev/null; then
+    # generate JSON files with LLMs
+    bash "$WORKING_DIR/llm-syscall/script/run.sh"
+fi
+
+# configure the environment variables
+source "$WORKING_DIR/safety-fuzzing/config/configure"
+
+# process JSON files to strace command options
+bash "$WORKING_DIR/scripts/process_json.sh"
+
+# build the syscall monitor image
+bash "$WORKING_DIR/safety-fuzzing/scripts/build_monitor_image.sh"
+
+# build the wrapped application image
+bash "$WORKING_DIR/safety-fuzzing/scripts/build_test_image.sh"
+
+# overwrite environment variables for the testbed
+export CONFIG_DIR="$TESTBED_PATH/examples/config"
+
+# run error injection 
+bash "$WORKING_DIR/safety-fuzzing/scripts/run.sh"
+
+# overwrite environment variables for the testbed
+export CONFIG_DIR="$WORKING_DIR/data/config/success/gpt-4o/run1"
+
+# run error injection 
+bash "$WORKING_DIR/safety-fuzzing/scripts/run.sh"
+
