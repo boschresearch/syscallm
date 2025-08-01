7ce0c85aa6676579580042a8a8805ce719db7e27
Jo Min Hee <MinHee.Jo@de.bosch.com>
2025-08-01 11:00:52 +0200

    Removes unused file counting logic and import

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,3 @@
-import os
 import json
 import utils.config as config
 
@@ -12,10 +11,3 @@ def is_json(file_path):
         return True
     except (ValueError, json.JSONDecodeError):
         return 
-    
-    
-def get_total_count(base_dir):
-    """Get the total count of JSON files in a directory."""
-    one_set = os.path.join(base_dir, 'json', models[0], 'run1')
-
-    return len(os.listdir(one_set))
