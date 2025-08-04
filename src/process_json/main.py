54b69b28c147752d17491aa6a5d5c51322277dfa
Jo Min Hee <MinHee.Jo@de.bosch.com>
2025-08-04 16:38:27 +0200

    Filter out out of bound values generated from the LLM

--- src/process_json/main.py
+++ src/process_json/main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import filter_out_of_bound
 import inject_what
 import filter_strace
 import inject_when
@@ -39,9 +40,12 @@ if __name__ == "__main__":
         if os.path.exists(dir_path):
             os.system(f"rm -r {dir_path}")
 
-    inject_what.process_all_models(json_dir_path, mode)
-    filter_strace.process_all_models(strace_dir_path, aut)
-    inject_when.process_all_models(strace_dir_path, aut)
-    strace_to_config.process_all_models(strace_dir_path)
-    random_config.process_all_models(config_dir_path, mode, "uniform")
-    random_config.process_all_models(config_dir_path, mode, "log")
+    print("1. Processing JSON files that have out of bound values...")
+    filter_out_of_bound.process_all_temperatures(directory=json_dir)
+
+    # inject_what.process_all_models(json_dir_path, mode)
+    # filter_strace.process_all_models(strace_dir_path, aut)
+    # inject_when.process_all_models(strace_dir_path, aut)
+    # strace_to_config.process_all_models(strace_dir_path)
+    # random_config.process_all_models(config_dir_path, mode, "uniform")
+    # random_config.process_all_models(config_dir_path, mode, "log")
