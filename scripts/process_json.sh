987fc1d3685ac0ab6959f1fe97f77dfeb1033b5f
Jo Min Hee <MinHee.Jo@de.bosch.com>
2025-08-12 11:04:38 +0200

    Remove obsolete input parameter

--- scripts/process_json.sh
+++ scripts/process_json.sh
@@ -6,4 +6,4 @@ WORKING_DIR="$(dirname "$SCRIPT_DIR")"
 ### assumption: the json file is already created on the GPU
 
 # convert json files to strace command options
-python3 ${WORKING_DIR}/src/process_json/main.py --aut=$APPLICATION_NAME
+python3 ${WORKING_DIR}/src/process_json/main.py
