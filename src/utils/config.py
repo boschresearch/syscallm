bdcd01a63351c63dcd95653ea9c43fb4f12883a8
Jo Min Hee <MinHee.Jo@de.bosch.com>
2025-11-21 14:27:03 +0100

    Delete obsolete variables

--- src/utils/config.py
+++ src/utils/config.py
@@ -13,15 +13,3 @@ baseline = "log"
 
 # file paths
 data_dir = "/home/jom8be/workspace/syscallm/data"
-
-# TODO: data are missing
-# file paths for each Ubuntu version
-kernel_5_4_0 = "/home/jom8be/workspace/syscallm/data/syscall/syscalls_5.4.0.txt"
-kernel_5_15_0 = "/home/jom8be/workspace/syscallm/data/syscall/syscalls_5.15.0.txt"
-kernel_6_8_0 = "/home/jom8be/workspace/syscallm/data/syscall/syscalls_6.8.0.txt"
-kernel_6_11_0 = "/home/jom8be/workspace/syscallm/data/syscall/syscalls_6.11.0.txt"
-
-# file paths for missing syscalls
-missing_5_15_0 = "/home/jom8be/workspace/syscallm/data/syscall/missing_syscalls_5.15.0.txt"
-missing_6_8_0 = "/home/jom8be/workspace/syscallm/data/syscall/missing_syscalls_6.8.0.txt"
-missing_6_11_0 = "/home/jom8be/workspace/syscallm/data/syscall/missing_syscalls_6.11.0.txt"
